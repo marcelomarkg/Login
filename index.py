2ba225cc21440609e93e5760c0462f5093e527ea
Marcelo Inacio <marcelomarkg@gmail.com>
2022-05-22 04:47:22 -0300

    FinalBack

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 #importar as bibliotecas
+from ast import Pass
+from csv import excel
+from email import message
 from email.mime import image
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 from turtle import back, left, width
 from webbrowser import BackgroundBrowser
+import DataBase
+
 
 #Criar janela
 jan = Tk()
@@ -41,8 +46,24 @@ PassLabel.place(x=5, y=150)
 PassEntry = ttk.Entry(RightFrame, width=30, show="*")
 PassEntry.place(x=149, y=160)
 
+def Login():
+    User = UserEntry.get()
+    Pass = PassEntry.get()
+
+    DataBase.cursor.execute("""
+    SELECT * FROM Users
+    WHERE (User = ? AND Password = ?)
+    """, (User, Pass))
+    print("Selecionou")
+    VerifyLogin = DataBase.cursor.fetchone()
+    try:
+        if (User in VerifyLogin and Pass in VerifyLogin):
+            messagebox.showinfo(title="Login info", message="Acesso confirmado, Bem vindo!")
+    except:
+        messagebox.showinfo(title="Login info", message="Acesso Negado. Verifique os dados")
+
 #Botoes
-LoginButton= ttk.Button(RightFrame, text="Login", width=30)
+LoginButton= ttk.Button(RightFrame, text="Login", width=30, command=Login)
 LoginButton.place(x=100, y=225)
 
 def Register():
@@ -55,16 +76,31 @@ def Register():
     NomeLabel = Label(RightFrame, text="Name:", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
     NomeLabel.place(x=5, y=5)
 
-    NomeEntry = Entry(RightFrame, width=39)
+    NomeEntry = ttk.Entry(RightFrame, width=39)
     NomeEntry.place(x=99, y=16)
 
     Emaillabel = Label(RightFrame, text="Email:", font=("Century Gothic", 20), bg="MIDNIGHTBLUE", fg="white")
     Emaillabel.place(x=5, y=55)
 
-    EmailEntry = Entry(RightFrame, width=39)
+    EmailEntry = ttk.Entry(RightFrame, width=39)
     EmailEntry.place(x=99, y=66)
 
-    Register = ttk.Button(RightFrame, text="Register", width=30)
+    def RegisterToDataBase():
+        Name = NomeEntry.get()
+        Email = EmailEntry.get()
+        User = UserEntry.get()
+        Pass = PassEntry.get()
+
+        if (Name == "" and Email == "" and User == "" and Pass == ""):
+            messagebox.showerror(title="Register Error", message="Preencha todos os campos. Não deixe nenhum vazio!")
+        else:            
+            DataBase.cursor.execute("""
+            INSERT INTO Users(Name, Email, User, Password) VALUES (?,?,?,?)
+            """, (Name, Email, User, Pass))
+            DataBase.conn.commit()
+            messagebox.showinfo(title="Register Info", message="Conta criada com sucesso")
+
+    Register = ttk.Button(RightFrame, text="Register", width=30, command=RegisterToDataBase)
     Register.place(x=100, y=225)
 
     def BackToLogin():
